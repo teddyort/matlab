c35349fc63992f05b6ff7097e09f27323fba05e0
Teddy Ort <teddy@mit.edu>
2016-11-20 03:34:21 -0500

    Adds all and any functions for arbitrary dimensions

---
+++ +teddy/all.m
@@ -0,0 +1,5 @@
+function [ result ] = all( A )
+% Like the regular all function but acts along all dimensions
+result = all(A(:));
+end
+
---
+++ +teddy/any.m
@@ -0,0 +1,6 @@
+function [ result ] = any( A )
+% Like the regular any function but acts along all dimensions
+% See: doc any
+result = any(A(:));
+end
+
