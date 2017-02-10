6b98c1e24a0a3c0a362bdcad044d2770dac71784
Teddy Ort <teddy@mit.edu>
2017-02-10 12:19:02 -0500

    Fixes row col symbols for lyx

--- teddy/mat2lyx.m
+++ teddy/mat2lyx.m
@@ -1,7 +1,8 @@
 function [ s ] = mat2lyx( A )
 % Formats the matrix A into a string capable of being pasted into a lyx
-% matrix.
+% matrix. 
+% Example: clipboard('copy',mat2lyx(A)) copies a lyx string for matrix A
 ajoin = @(f,v,glue) strjoin(arrayfun(f, v,'uniformoutput',0),glue);
-s=ajoin(@(r) ajoin(@(x) num2str(x),A(r,:),'\\\'), 1:size(A,1),'&');
+s=ajoin(@(r) ajoin(@(x) num2str(x),A(r,:),'&'), 1:size(A,1),'\\\');
 end
 
