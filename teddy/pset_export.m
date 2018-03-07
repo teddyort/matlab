1e817e59ad41c265fb4cba9f30ee79704caf7f5a
Teddy Ort <teddy@mit.edu>
2018-03-07 13:55:27 -0500

    Better pset_export

--- teddy/pset_export.m
+++ teddy/pset_export.m
@@ -1,9 +1,22 @@
 function pset_export(fname)
 %Exports the current figure in a consistent manner to a folder named
 %'Resources' in the current location in png and fig formats
-    p=get(gcf,'Position');
-    set(gcf,'Position',[p(1) p(2) 800 800]);
-    savefig(['Resources/' fname]);
-    saveas(gcf,['Resources/' fname '.png']);
-    set(gcf,'Position', p);
+    fs_bak = get(gca,'fontsize');
+    f = gcf;
+    pos_bak = f.Position;
+    f.Position(end)=f.Position(end-1);
+    set(gca,'fontsize',18)
+    ppi=get(0,'ScreenPixelsPerInch');
+    f.PaperUnits='inches';
+    f.PaperSize = f.Position(3:4)/ppi;
+    f.PaperPosition = [0 0 f.Position(3:4)/ppi];
+    if exist([cd '\Resources'],'dir')==7
+        folder = 'Resources';
+    else
+        folder = uigetdir;
+    end
+    savefig([folder '\' fname]);
+    print(gcf,[folder '\' fname '.png'],'-dpng', '-r300');
+    f.Position = pos_bak;
+    set(gca, 'FontSize', fs_bak);
 end
